c036011bf2efe8e1ec66eb42454ef16bff1ad078
Luning39 <luning12418@qq.com>
2025-12-17 23:35:51 +0800

    refactor: simplify LanternSwitch interaction logic

--- typescripts/customComponents/LanternSwitch.ts
+++ typescripts/customComponents/LanternSwitch.ts
@@ -1,15 +1,13 @@
-import { world, Block, PlayerInteractWithBlockAfterEvent, Player, ItemStack } from "@minecraft/server";
+import { world, BlockPermutation, PlayerInteractWithBlockAfterEvent } from "@minecraft/server";
 import { methodEventSub } from "../lib/eventHelper";
 
 export class LanternSwitch {
     @methodEventSub(world.afterEvents.playerInteractWithBlock)
-    itemUseOn(args: PlayerInteractWithBlockAfterEvent) {
-        const player = args.player;
+    interactWithBlock(args: PlayerInteractWithBlockAfterEvent) :void{
         const block = args.block;
-        const mainHand = args.itemStack;
         if (!block.hasTag("cfd:is_lantern")) return;
-        // block.setPermutation(block.permutation.withState("cfd:is_lighting",ture));
-        if (mainHand.typeId === "minecraft:torch") {
-        }
+        const mainHand = args.itemStack;
+        if (!mainHand || mainHand.typeId !=="minecraft:torch") return;
+        block.setPermutation(block.permutation.withState('cfd:is_lighting', true));
     }
 }
